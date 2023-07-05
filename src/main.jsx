86e5a07c8558e672f7d56191252b3f40ab696b9d
osmangony01 <mdosmangoni376@gmail.com>
2023-07-06 01:08:48 +0600

    create project setup

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Task from './components/Task.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Task />
+  </React.StrictMode>,
+)
